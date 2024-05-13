c05c5374e88e2bc861254b06098d74339a07a3fb
anton ivakhnenko <sa1094@gmail.com>
2024-05-13 19:13:54 +0300

    initial commit

--- precode.sh
+++ precode.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 # создаём каталог task с вложенными директориями
 # task
@@ -6,38 +7,52 @@
 #   dir2
 #   dir3
 #       dir4
+mkdir -p task/dir{1..3} && mkdir -p task/dir3/dir4
 
 # изменяем текущую директорию на task
+cd task
 
 # создаём пустой файл task/dir2/empty
+touch dir2/empty
 
 # создаём файл task/dir2/hello.sh с таким содержанием:
 # #!/bin/bash
 # echo "$1, привет!"
 
+cat > dir2/hello.sh <<\EOL
+#!/bin/bash
+echo "$1, привет!"
+EOL
+
+
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
+chmod 764 dir2/hello.sh
 
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
+ls dir2 > dir2/list.txt
 
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
-
+cp -rT dir2/ dir3/dir4
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
-
+find ./ -name *.txt > dir1/summary.txt 2>/dev/null
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
-
+cat dir2/list.txt  >> dir1/summary.txt 
 # определяем переменную окружения NAME со значением "Всем студентам"
-
+NAME="Всем студентам"
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
-
+dir2/hello.sh "$NAME" >> dir1/summary.txt
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
-
+mv dir1/summary.txt "Практическое задание" 
 # выводим на консоль содержимое файла task/Практическое задание
-
+cat "Практическое задание"
 # ищем в файле "Практическое задание" строки, которые содержат слово "dir"
 # и затем отсортировываем их
+grep dir "Практическое задание" | sort 
 
 # меняем текущую директорию на родительскую для task
+cd ..
 
 # удаляем директорию task со всем содержимым
+rm -r task
